7f0db011c45f1256c5c16ad29fda430960021f5d
hungreo <hungnkb@gmail.com>
2022-12-19 23:40:34 +0700

    s

---
+++ main.ts
@@ -0,0 +1,14 @@
+let findItemMiss = (arr: number[]) => {
+    let i: number = 1;
+    let result: number[] = [];
+
+    while (i < (arr.length + 1)) {
+        if (arr.indexOf(i) == -1) {
+            result.push(i);
+        }
+        i++;
+    }
+    return result;
+}
+console.log(findItemMiss([1, 2, 5, 6, 7, 10]))
+
